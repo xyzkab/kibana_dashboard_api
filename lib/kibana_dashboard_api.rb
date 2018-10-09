ee151816f9a16102d3d0391a02e0b7622f5d3f39
xyzkab <0xyzkab@gmail.com>
2018-10-10 00:39:34 +0700

    copying self configuration to http/repeater

--- lib/kibana_dashboard_api.rb
+++ lib/kibana_dashboard_api.rb
@@ -5,4 +5,14 @@ module KibanaDashboardApi
   
   extend HTTP::Configuration::Mixin
 
+  def self.after_configure
+    # Copying `self` configuration to HTTP::Repeater
+    HTTP::Repeater.configure do |conf|
+      conf.base_uri.host = @configuration.base_uri.host
+      conf.base_uri.port = @configuration.base_uri.port
+      @configuration.base_headers.each do |key, val|
+        conf.base_headers[key] = val
+      end
+    end
+  end
 end

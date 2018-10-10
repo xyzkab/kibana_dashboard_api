c8279bad3f33acc84ced00e4aef739c7afcd6352
xyzkab <0xyzkab@gmail.com>
2018-10-10 16:34:47 +0700

    add :default_index attribute and :set_default_index method when saving new pattern

--- lib/kibana_dashboard_api/index/pattern.rb
+++ lib/kibana_dashboard_api/index/pattern.rb
@@ -20,26 +20,33 @@ module KibanaDashboardApi
         @type  = attributes[:type]
         @title = attributes[:attributes] ? attributes[:attributes][:title] : attributes[:title]
         @time_field_name = attributes[:time_field_name]
+        @default_index = attributes[:default_index] || false
       end
 
       def save
-        data   = {:attributes => {:title => @title, :timeFieldName => @time_field_name} }
+        data   = {:attributes => {:title => "#{@title}*", :timeFieldName => @time_field_name} }
         req    = HTTP::Repeater.post("/api/saved_objects/index-pattern", :json => data)
         @id    = req.json[:id]
         @type  = req.json[:type]
         @title = req.json[:attributes][:title]
         @time_field_name = req.json[:time_field_name]
+        set_default_index if @default_index
         self
       end
 
       def destroy
         req = HTTP::Repeater.delete("/api/saved_objects/index-pattern/#{@id}")
-        
+
         if req.status.code != 200
           raise HTTP::RequestError.new("id `#{@id}` not found")
         else
           true
         end
+      end
+
+      def set_default_index
+        req = HTTP::Repeater.post("/api/kibana/settings/defaultIndex", :json => {:value => @id})
+        req.json
       end
       
     end

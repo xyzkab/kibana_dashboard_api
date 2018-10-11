2a0100c3ae010fa3a663faa81e4cf4563f7535c1
xyzkab <0xyzkab@gmail.com>
2018-10-11 18:06:19 +0700

    add :set_date_format,:clear_date_format and default :dateFormat

--- lib/kibana_dashboard_api/settings.rb
+++ lib/kibana_dashboard_api/settings.rb
@@ -16,11 +16,24 @@ module KibanaDashboardApi
       return attributes[:settings]
     end
 
+    def self.set_date_format(value)
+      data = {:value => value}
+      req  = HTTP::Repeater.post("/api/kibana/settings/dateFormat", :json => data)
+
+      new(normalize_attribute(req.json))
+    end
+
+    def self.clear_date_format
+      req = HTTP::Repeater.delete("/api/kibana/settings/dateFormat")
+
+      new(normalize_attribute(req.json))
+    end
+
     attr_reader :default_index, :date_format
 
     def initialize(attributes = {})
       @default_index = attributes[:defaultIndex]
-      @date_format   = attributes[:dateFormat]
+      @date_format   = attributes[:dateFormat] || "MMMM Do YYYY, HH:mm:ss.SSS " # Kibana Default
     end
   end
   
